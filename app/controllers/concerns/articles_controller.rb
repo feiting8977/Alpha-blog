986663620d122bf7805afeb4f7dcef9d8fedecce
ting fei <appcareer2018@gmail.com>
2018-06-03 23:04:14 +0000

    Style errors and message

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
         # render plain: params[:article].inspect
         @article = Article.new(article_params)
         if @article.save 
-            flash[:notice] = "Your article has been successfully created!"
+            flash[:success] = "Your article has been successfully created!"
             redirect_to article_path(@article)
         else
             render 'new'
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
     
     def update
         if @article.update(article_params)
-            flash[:notice] = "Article has been updated!"
+            flash[:success] = "Article has been updated!"
             redirect_to article_path(@article)
         else 
             render 'edit'
@@ -40,7 +40,7 @@ class ArticlesController < ApplicationController
     
     def destroy
         @article.destroy
-        flash[:notice] = "Your article has been successfully deleted!"
+        flash[:danger] = "Your article has been successfully deleted!"
         
         redirect_to articles_path
         
